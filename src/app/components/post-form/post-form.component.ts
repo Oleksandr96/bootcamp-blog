f20defbb93b3d80b42ba89821b7649baf01a7a02
Oleksandr Bukhanenko <oleksandr.bukhanenko@intellias.com>
2021-12-09 00:33:07 +0200

    update form validation

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { AppPostsService } from '../../services/app-posts.service';
 import { Post } from '../../interfaces/post.interface';
+import { debounceTime, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-form',
@@ -11,6 +12,28 @@ import { Post } from '../../interfaces/post.interface';
 })
 export class PostFormComponent implements OnInit {
   newPostForm!: FormGroup;
+  newPostFormSubscription!: Subscription;
+
+  newPostFormErrors: any = {
+    author: null,
+    title: null,
+    text: null,
+  };
+
+  validationMessages: any = {
+    author: {
+      required: 'Field required',
+      minlength: 'The minimum field length is 3 characters.',
+    },
+    title: {
+      required: 'Field required',
+      minlength: 'The minimum field length is 5 characters.',
+    },
+    text: {
+      required: 'Field required',
+      minlength: 'The minimum field length is 5 characters.',
+    },
+  };
 
   constructor(private appPostsService: AppPostsService) {}
 
@@ -21,7 +44,24 @@ export class PostFormComponent implements OnInit {
       this.newPostForm.reset();
     }
   }
+  onInputChange() {
+    if (!this.newPostForm.valid) {
+      Object.keys(this.newPostFormErrors).forEach((field: string) => {
+        this.newPostFormErrors[field] = '';
+        let formInput = this.newPostForm.get(field);
 
+        if (!formInput!.valid && formInput!.dirty) {
+          let errorMessage = this.validationMessages[field];
+
+          Object.keys(formInput?.errors!).forEach((key: string) => {
+            if (errorMessage[key]) {
+              this.newPostFormErrors[field] = errorMessage[key];
+            }
+          });
+        }
+      });
+    }
+  }
   ngOnInit(): void {
     this.newPostForm = new FormGroup({
       author: new FormControl('', [
@@ -30,12 +70,12 @@ export class PostFormComponent implements OnInit {
       ]),
       title: new FormControl('', [
         Validators.required,
-        Validators.minLength(10),
+        Validators.minLength(5),
       ]),
-      text: new FormControl('', [
-        Validators.required,
-        Validators.minLength(30),
-      ]),
+      text: new FormControl('', [Validators.required, Validators.minLength(5)]),
     });
+    this.newPostFormSubscription = this.newPostForm.valueChanges
+      .pipe(debounceTime(400))
+      .subscribe((x) => this.onInputChange());
   }
 }
