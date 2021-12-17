63e88116ec33ebd6ec2899f7bb58f959af2fba36
Oleksandr Bukhanenko <oleksandr.bukhanenko@intellias.com>
2021-12-17 16:27:46 +0200

    Errors handler implementation

--- src/backend/controllers/posts-list.js
+++ src/backend/controllers/posts-list.js
@@ -1,5 +1,10 @@
 const postsData = require("../data/posts-data.json");
+const errorHandler = require("../utils/errorHandler");
 
 module.exports.getAll = async (req, res) => {
-  res.status(200).send(postsData);
+  try {
+    res.status(200).send(postsData);
+  } catch (e) {
+    errorHandler(res, e);
+  }
 };
--- src/backend/routes/posts-list.js
+++ src/backend/routes/posts-list.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/posts", controller.getAll);
 router.get("*", (req, res) => {
-  res.status(404).send("404 - Not found");
+  res.status(404).json({ message: "Not found" });
 });
 
 module.exports = router;
