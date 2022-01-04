06dc09c662489d253b086c97a9d3fabc1bdecd4e
Oleksandr Bukhanenko <oleksandr.bukhanenko@intellias.com>
2022-01-04 02:20:38 +0200

    feat - post page component styles, main styles updating

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -7,6 +7,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatChipsModule } from '@angular/material/chips';
 
 const materialModules = [
   MatDialogModule,
@@ -17,6 +18,7 @@ const materialModules = [
   MatToolbarModule,
   MatCardModule,
   MatProgressBarModule,
+  MatChipsModule,
 ];
 
 @NgModule({
